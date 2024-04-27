caad8cd5ab083e26f7354d8bf49a20bfadc96f20
Max <>
2024-04-27 15:44:26 +0200

    First_commit

---
+++ blackjack/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace blackjack
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Pustoy;
+
+            safasfaf; 
+        }
+    }
+}
